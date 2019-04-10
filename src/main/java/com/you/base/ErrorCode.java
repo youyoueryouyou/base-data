9004d6c9d28207111d79b20355523aa4f7d4f1f7
admin <you@you.com>
2019-04-10 17:36:56 +0800

    update

--- src/main/java/com/you/base/ErrorCode.java
+++ src/main/java/com/you/base/ErrorCode.java
@@ -1,7 +1,7 @@
 package com.you.base;
 
 public enum ErrorCode {
-    COMMAN_ERROR(-1),PARAM_EMPTY(1),PARAM_ERROR(2);
+    COMMAN_ERROR(-1),PARAM_EMPTY(1),PARAM_ERROR(2),NO_ACCESS(3),NO_LOGIN(4);
     private int code;
     ErrorCode(int code){
      this.code = code;
